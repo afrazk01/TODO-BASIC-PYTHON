3ae0fc7e6a2f34e245f5e0cd513a8294238b2955
Afraz Khan <afrazkhan0714@gmail.com>
2025-05-21 15:09:45 +0500

    added file implementation

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 This is basic Practice TODO App
-## Day 01 & 2
-Basic implementation of TODO App using simple list and match cases and loop 
--- main.py
+++ main.py
@@ -1,24 +1,47 @@
-todo_list = []
-
 while True:
-    user_input = input("Enter a command (add, remove, show, quit): ").strip().lower()
+    user_input = input("Enter a command (add, show, remove, edit, complete, quit): ").strip().lower()
+
     match user_input:
+
         case "show":
-            for item in todo_list:
-                print(item)
+            with open("files/todo.txt", "r") as file:
+                todo_list = file.readlines()
+                print("This is how readlines looks like:", todo_list)
+                for index, item in enumerate(todo_list):
+                    print(f"{index+1}: {item.strip()}")
+
         case "add":
-            item = input("Enter the item to add: ").strip()
-            todo_list.append(item)
+            item = input("Enter the task to add: ").strip()
+            with open("files/todo.txt", "a") as file:
+                file.write(item + "\n")
             print(f"Added '{item}' to the list.")
-        case "remove":
-            print("These are the Item available to remove:")
-            for item in todo_list:
-                print(item)
-            item = input("Enter the item to remove: ").strip()
-            if item in todo_list:
-                todo_list.remove(item)
-                print(f"Removed '{item}' from the list.")
-            else:
-                print(f"'{item}' not found in the list.")
+
+        case "remove" | "complete":
+            with open("files/todo.txt", "r") as file:
+                todo_list = file.readlines()
+            for index, item in enumerate(todo_list):
+                print(f"{index+1}: {item.strip()}")
+            task_no = int(input("Select the task number to remove: "))
+            todo_list.pop(task_no - 1)
+            with open("files/todo.txt", "w") as file:
+                file.writelines(todo_list)
+            print("Task removed.")
+
+        case "edit":
+            with open("files/todo.txt", "r") as file:
+                todo_list = file.readlines()
+            for index, item in enumerate(todo_list):
+                print(f"{index+1}: {item.strip()}")
+            task_no = int(input("Select the task number to edit: "))
+            new_item = input("Enter the new task: ").strip()
+            todo_list[task_no - 1] = new_item + "\n"
+            with open("files/todo.txt", "w") as file:
+                file.writelines(todo_list)
+            print("Task updated.")
+
         case "quit":
+            print("Goodbye!")
             break
+
+        case _:
+            print("Invalid command. Try again.")
